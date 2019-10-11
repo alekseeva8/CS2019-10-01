dff6ada3ac25d5b8ababa2317176f44a1197358b
alekseeva8 <alekseeva.ev8@gmail.com>
2019-10-11 13:44:46 +0300

    lesson04 Task A

--- src/by/it/alekseeva/lesson04/TaskA2.java
+++ src/by/it/alekseeva/lesson04/TaskA2.java
@@ -24,6 +24,23 @@ package by.it.alekseeva.lesson04;
 
  */
 
+import com.sun.deploy.security.SelectableSecurityManager;
+
+import java.util.Scanner;
+
 public class TaskA2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        if (i == 1) System.out.println("понедельник");
+        else if (i == 2) System.out.println("вторник");
+        else if (i == 3) System.out.println("среда");
+        else if (i == 4) System.out.println("четверг");
+        else if (i == 5) System.out.println("пятница");
+        else if (i == 6) System.out.println("суббота");
+        else if (i == 7) System.out.println("воскресенье");
+        else
+            System.out.println("такого дня недели не существует");
+    }
 
 }
